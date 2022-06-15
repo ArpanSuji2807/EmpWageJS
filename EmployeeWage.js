de5f3f9827517323082d478e750962b63c0d77fb
Praan26 <arpanap102@gmail.com>
2022-06-15 13:12:26 +0530

    [Arpan] Add: Working hours added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -10,25 +10,27 @@
         console.log("Employee is Present");
     }
 }
-//UC2- DailyWage of Part time or Full time
+//UC2- DailyWage of Part time or Full time &  UC3- Get woking hours
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 {
-    let empHrs = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
+    function getWorkingHrs(empCheck) {
     switch(empCheck)
     {
         case IS_PART_TIME:
-            empHrs = PART_TIME_HRS;
-            break;
+            return PART_TIME_HRS;
         case IS_FULL_TIME:
-            empHrs = FULL_TIME_HRS;
+            return FULL_TIME_HRS;
         default:
-            empHrs = 0;
+            return 0;
     }
+    }
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHrs(empCheck);
     let empWage = empHrs*WAGE_PER_HR;
+    console.log("Working hours: "+empHrs);
     console.log("Employee Wage is: "+empWage);
 }
