e733bdf9a1e6fda07d5f4ab9d7e1e35c4a51a8db
Praan26 <arpanap102@gmail.com>
2022-06-15 18:04:04 +0530

    [Arpan] Add: Daily Wage stored in an Array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -18,9 +18,7 @@ const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const NUMBER_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
-let totalEmpHrs = 0;
 let empHrs = 0;
-let totalWorkingDays = 0;
 {
     function getWorkingHrs(empCheck) {
     switch(empCheck)
@@ -33,14 +31,21 @@ let totalWorkingDays = 0;
             return 0;
     }
     }
+    function calcDailyWage(empHrs){
+        return empHrs * WAGE_PER_HR
+    }
 
+    let empDailyWageArray = new Array();
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
     while(totalEmpHrs<MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
     {
         totalWorkingDays++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
         empHrs=getWorkingHrs(empCheck);
         totalEmpHrs += empHrs;
+        empDailyWageArray.push(calcDailyWage(empHrs));
     }
-    let empWage = totalEmpHrs*WAGE_PER_HR;
+    let empWage =calcDailyWage(totalEmpHrs);
     console.log("Total Days: "+totalWorkingDays+ "Total Hours: "+totalEmpHrs+ "Employee Wage is: "+empWage);
 }
